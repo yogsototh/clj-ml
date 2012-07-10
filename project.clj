72bae7701a0c1f057f3445c2bb9ef971d06363c6
Ben Mabey <ben@benmabey.com>
2012-07-09 18:06:31 -0600

    fixes weka's groupid to avoid maven warnings

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.leadtune/clj-ml "0.2.3"
+(defproject com.leadtune/clj-ml "0.2.4"
   :description "Machine Learning library for Clojure built around Weka and friends"
   :repositories {"leadtune-repo" "http://utility.leadtune.com:8081/nexus/content/repositories/thirdparty/"}
   :aoc "src/java"
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [incanter/incanter-core "1.3.0"]
                  [incanter/incanter-charts "1.3.0"]
-                 [lt/weka "3.6.3"]
+                 [cs.waikato.ac.nz/weka "3.6.3"]
                  [hr.irb/fastRandomForest "0.98"]]
   :dev-dependencies [[midje "1.3.0-RC2"]
                      [swank-clojure "1.3.4"]])
