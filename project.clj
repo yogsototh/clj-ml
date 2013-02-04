83486746efe25d467d636065d6f0b93d0d68284a
Ben Mabey <ben@benmabey.com>
2013-02-04 08:55:36 -0700

    moves to lein2

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-(defproject com.leadtune/clj-ml "0.2.4"
+(defproject com.leadtune/clj-ml "0.2.5-SNAPSHOT"
   :description "Machine Learning library for Clojure built around Weka and friends"
-  :repositories {"leadtune-repo" "http://utility.leadtune.com:8081/nexus/content/repositories/thirdparty/"}
-  :aoc "src/java"
-  :javac-fork "true"
+  :repositories {"leadtune-repo" "http://utility.leadtune.com:8081/nexus/content/repositories/thirdparty/"
+                 "stuart" "http://stuartsierra.com/maven2"}
+  :java-source-paths ["src/java"]
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [incanter/incanter-core "1.3.0"]
                  [incanter/incanter-charts "1.3.0"]
                  [cs.waikato.ac.nz/weka "3.6.3"]
                  [hr.irb/fastRandomForest "0.98"]]
-  :dev-dependencies [[midje "1.3.0-RC2"]
-                     [swank-clojure "1.3.4"]])
+  :profiles {:dev
+             {:plugins [[lein-midje "2.0.0"]]
+              :dependencies [[midje "1.4.0"]
+                             [swank-clojure "1.4.3"]
+                             [com.stuartsierra/lazytest "1.2.3"]]}})
