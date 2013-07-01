123cd1713cda84426694f88eb5da25f147e06d08
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-07-01 14:40:05 -0400

    Switched to a modern Snowball stemmer implementation. Bumped version to 0.3.9.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.artifice/clj-ml "0.3.8"
+(defproject cc.artifice/clj-ml "0.3.9"
   :description "Machine Learning library for Clojure built around Weka and friends"
   :java-source-paths ["src/java"]
   :license {:name "MIT License"
@@ -9,7 +9,7 @@
                  [incanter/incanter-charts "1.4.1"]
                  [nz.ac.waikato.cms.weka/weka-stable "3.6.9"]
                  [tw.edu.ntu.csie/libsvm "3.1"]
-                 [org.apache.lucene/lucene-snowball "3.0.3"]
+                 [org.apache.lucene/lucene-analyzers-common "4.3.0"]
                  [org.clojars.chapmanb/fast-random-forest "0.98"]]
   :profiles {:dev
              {:plugins [[lein-midje "2.0.0"]]
