623e9a1ef37bee64d40b55a2db02733f69fd930f
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-07-04 09:34:53 -0400

    New format for terms on public datasets. Bumped to version 0.3.12.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.artifice/clj-ml "0.3.11"
+(defproject cc.artifice/clj-ml "0.3.12"
   :description "Machine Learning library for Clojure built around Weka and friends"
   :java-source-paths ["src/java"]
   :license {:name "MIT License"
