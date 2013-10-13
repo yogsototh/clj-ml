c220b141db0aab3fd4b94373105cc69129626c7b
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-10-12 21:30:57 -0400

    Move to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject cc.artifice/clj-ml "0.4.0"
+(defproject cc.artifice/clj-ml "0.5.0-SNAPSHOT"
   :description "Machine Learning library for Clojure built around Weka and friends"
   :java-source-paths ["src/java"]
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :url "https://github.com/joshuaeckroth/clj-ml"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [nz.ac.waikato.cms.weka/weka-stable "3.6.9"]
+  :dependencies [[nz.ac.waikato.cms.weka/weka-stable "3.6.9"]
                  [tw.edu.ntu.csie/libsvm "3.1"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.apache.lucene/lucene-analyzers-common "4.3.0"]]
