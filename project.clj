93d688de8ebfe18acef99cc6615c73460a061d26
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2014-05-02 09:05:53 -0400

    Updated dependencies and added official snowball stemmer dependency.

--- project.clj
+++ project.clj
@@ -4,22 +4,22 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :url "https://github.com/joshuaeckroth/clj-ml"
-  :dependencies [[nz.ac.waikato.cms.weka/weka-dev "3.7.10"]
-		 [nz.ac.waikato.cms.weka/chiSquaredAttributeEval "1.0.3"]
-		 [nz.ac.waikato.cms.weka/attributeSelectionSearchMethods "1.0.6"]
-		 [nz.ac.waikato.cms.weka/linearForwardSelection "1.0.1"]
+  :dependencies [[nz.ac.waikato.cms.weka/weka-dev "3.7.11"]
+		 [nz.ac.waikato.cms.weka/chiSquaredAttributeEval "1.0.4"]
+		 [nz.ac.waikato.cms.weka/attributeSelectionSearchMethods "1.0.7"]
+		 [nz.ac.waikato.cms.weka/linearForwardSelection "1.0.2"]
 		 [nz.ac.waikato.cms.weka/rotationForest "1.0.3"]
 		 [nz.ac.waikato.cms.weka/paceRegression "1.0.2"]
 		 [nz.ac.waikato.cms.weka/SPegasos "1.0.2"]
-		 [nz.ac.waikato.cms.weka/LibSVM "1.0.5"]
-                 [org.clojars.remleduff/snowball "20051019-1"]
+		 [nz.ac.waikato.cms.weka/LibSVM "1.0.6"]
 		 [junit/junit "4.11"]
-                 [tw.edu.ntu.csie/libsvm "3.1"]
+                 [tw.edu.ntu.csie/libsvm "3.17"]
                  [org.clojure/data.xml "0.0.7"]
-                 [org.apache.lucene/lucene-analyzers-common "4.3.0"]]
+                 [org.apache.lucene/lucene-analyzers-common "4.8.0"]
+                 [org.apache.lucene/lucene-snowball "3.0.3"]]
   :profiles {:dev
              {:plugins [[lein-midje "2.0.0"]]
-              :dependencies [[midje "1.4.0"]]}}
+              :dependencies [[midje "1.6.3"]]}}
   :codox {:output-dir "website/doc"
           :src-dir-uri "http://github.com/joshuaeckroth/clj-ml/blob/master"
           :src-linenum-anchor-prefix "L"})
